855cb9b8a1e8db8d8d9f2d18c0c044104555c60f
Philipe Coelho <93295875+philipecoelhoo@users.noreply.github.com>
2023-10-26 22:12:49 -0300

    Atualização atributos HTML com JS

--- Script.js
+++ Script.js
@@ -1,5 +1,10 @@
 function toggleMode() {
     const html = document.documentElement;
-
     html.classList.toggle('light');
-}
+
+    const img = document.querySelector("#profile img");
+
+    if (html.classList.contains('light')) {
+        img.setAttribute('src', './assets/assets/avatar-light-phil.png');
+    } else {
+        img.setAttribute('src', './assets/assets/avatar_phil.png');}}
